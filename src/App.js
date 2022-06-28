f5b5cac697b22a044cc0d6b93edc56b269653fe7
oscar <meet@oscarfdez.com>
2022-06-28 10:37:59 +0200

    my first commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          ¡Hey!
+        </h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/OschubGit/onlineshop-fdez"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go to Github
         </a>
       </header>
     </div>
