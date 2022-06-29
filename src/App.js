0a13f12e32a4410c21cbf95601bcd7ebd4f3c4ab
oscar <meet@oscarfdez.com>
2022-06-29 12:22:30 +0200

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
 import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import {Container} from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          ¡Hey!
-        </h1>
-        <a
-          className="App-link"
-          href="https://github.com/OschubGit/onlineshop-fdez"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Go to Github
-        </a>
+          <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center', flexGrow: 1 }}>
+            <nav className='nav'>
+              <div className='nav-menu'>
+                <Typography sx={{ minWidth: 100 }}>Home</Typography>
+                <Typography sx={{ minWidth: 100 }}>Shop</Typography>
+              </div>
+              <div className='btn-login'>
+                <Button variant='outlined' color="inherit">Login</Button>
+              </div>
+            </nav>
+          </Box>
+            </Toolbar>
+          </AppBar>
+        </Box>
       </header>
+      <Container className='container'>
+        <Typography variant='h1'>
+          ¡Hey!
+        </Typography>
+      </Container>
     </div>
   );
 }
